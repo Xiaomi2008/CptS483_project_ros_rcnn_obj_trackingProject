eca2fe3419f009016103fe41005413d7ba2d8c36
Tao Zeng <zentao08@gmail.com>
2016-12-07 14:13:27 -0800

    working code for rcnn_ros

--- rcnn_object_tracing.py
+++ rcnn_object_tracing.py
@@ -6,7 +6,7 @@ from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
 import scipy.io as sio
 import caffe, os, sys, cv2
-#import argparse
+import argparse
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -18,7 +18,8 @@ import tf
 import PyKDL as kdl
 from intro_to_robotics.image_converter import ToOpenCV, depthToOpenCV
 
-
+global Net
+Net = 0
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
            'bottle', 'bus', 'car', 'cat', 'chair',
@@ -38,6 +39,44 @@ from geometry_msgs.msg import Point, Quaternion
 #roslib.load_manifest('odom_publisher')
 PI=3.14159265359
 Mag_stop_size =8000000 # size of fire hydrant
+def vis_opencv_detections(im, class_name, dets, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    if len(inds) == 0:
+        return
+
+
+    for i in inds:
+        bbox = dets[i, :4].astype(int)
+        score = dets[i, -1]
+        cv2.rectangle(im, (bbox[0],  bbox[1]), ( bbox[2],bbox[3]), (0,0,255), 1)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(bbox[0], bbox[1] - 2), font, 0.5,(255,255,255),1)
+
+
+# def demo_video(net,v_f):
+#     v_file=os.path.join(cfg.DATA_DIR,'demo',v_f)
+#     cap = cv2.VideoCapture(v_file)
+#     CONF_THRESH = 0.8
+#     NMS_THRESH = 0.3
+#     while(cap.isOpened()):
+#         ret, frame = cap.read()
+#
+#         scores, boxes = im_detect(net, frame)
+#         #cv2.imshow('frame',frame)
+#         for cls_ind, cls in enumerate(CLASSES[1:]):
+#             cls_ind += 1 # because we skipped background
+#             cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+#             cls_scores = scores[:, cls_ind]
+#             dets = np.hstack((cls_boxes,
+#                               cls_scores[:, np.newaxis])).astype(np.float32)
+#             keep = nms(dets, NMS_THRESH)
+#             dets = dets[keep, :]
+#         #im = np.zeros((512,512,3), np.uint8)
+#             vis_opencv_detections(frame, cls, dets, thresh=CONF_THRESH)
+#         #vis_detections(im, cls, dets, thresh=CONF_THRESH)
+#     #display the original image with the centroid drawn on the image
+#         cv2.imshow("processing result", frame)
 
 #this function does our image processing
 #returns the location and "size" of the detected object
@@ -73,29 +112,29 @@ def process_image(image):
     #waitKey() is necessary for making all the cv2.imshow() commands work
     cv2.waitKey(1)
     return location, magnitude
-	
+
 
 def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
-	
-	
-	
-    
+
+
+
+
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
-		cv2.rectangle(im, (bbox[0],  bbox[1]), ( bbox[2],bbox[3]), (0,0,255), 2)
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(bbox[0], bbox[1] - 2), font, 0.5,(0,0,255),2)
+	cv2.rectangle(im, (bbox[0],  bbox[1]), ( bbox[2],bbox[3]), (0,0,255), 2)
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(bbox[0], bbox[1] - 2), font, 0.5,(0,0,255),2)
 	#display the original image with the centroid drawn on the image
-    cv2.imshow("processing result", im)
+    #cv2.imshow("processing result", im)
 
     #waitKey() is necessary for making all the cv2.imshow() commands work
-    cv2.waitKey(1)
-	
+    #cv2.waitKey(1)
+
         # ax.add_patch(
             # plt.Rectangle((bbox[0], bbox[1]),
                           # bbox[2] - bbox[0],
@@ -115,15 +154,33 @@ def vis_detections(im, class_name, dets, thresh=0.5):
     # plt.tight_layout()
     # plt.draw()
 def process_frame(im):
-
+    global Net
 	# scores shape =(N, 21) , wherer N is number of proposed regions, 21 is the number of class
 	# boxs  =(N,21*4) , every box has  4 coodinates (Left,top, righ, bottom)
-	
-	scores, boxes = im_detect(net, im)
+    if Net == 0:
+        Net=init_rcnn()
+    #im_file = os.path.join(cfg.DATA_DIR, 'demo', '001763.jpg')
+    #im = cv2.imread(im_file)
+    #scores, boxes = im_detect(net, im)
+    #print type(im)
+    #print (im.shape)
+    #print (net)
+    #cv2.imshow("processing result", im)
+    #cv2.waitKey(1)
+    #return
+
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(Net, im)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+    #scores, boxes = im_detect(net, im)
+    #vis_opencv_detections()
 	 # Visualize detections for each class
-    CONF_THRESH = 0.8
+    CONF_THRESH = 0.9
     NMS_THRESH = 0.3
-	cv2.imshow("processing result", image)
+	#cv2.imshow("processing result", image)
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
@@ -132,8 +189,9 @@ def process_frame(im):
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-		cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
-        #vis_detections(im, cls, dets, thresh=CONF_THRESH)
+        vis_opencv_detections(im, cls, dets, thresh=CONF_THRESH)
+    cv2.imshow("processing result", im)
+    cv2.waitKey(1)
 
 class Node:
     def __init__(self):
@@ -152,18 +210,28 @@ class Node:
     def image_callback(self, ros_image):
         # convert the ros image to a format openCV can use
         cv_image = np.asarray(ToOpenCV(ros_image))
+        process_frame(cv_image)
+        return
 
         #run our vision processing algorithm to pick out the object
         #returns the location (x,y) of the object on the screen, and the
         #"size" of the discovered object. Size can be used to estimate
         #distance
         #None/0 is returned if no object is seen
-        location, magnitude = process_image(cv_image)
+
+
+        #location, magnitude = process_frame(cv_image)
+
+
         #print magnitude
 
         #log the processing results
-        rospy.logdebug("image location: {}\tmagnitude: {}".format(location, magnitude))
-        self.reaction_controller(location,magnitude)
+
+        #rospy.logdebug("image location: {}\tmagnitude: {}".format(location, magnitude))
+        #self.reaction_controller(location,magnitude)
+
+
+
 
             #print location
             #print magnitude
@@ -181,15 +249,15 @@ class Node:
         if location == None:
             theta = PI/100
             self.turn(theta,0.15)
-        
+
         elif magnitude <Mag_stop_size and location != None:
             #print location
             #print magnitude
 
             theta = location[0]/30 * PI/8;
-        
-           
-            
+
+
+
             if abs(theta) >PI/8:
                 #print 'thetha is {}'.format(theta)
                 if theta>0:
@@ -254,8 +322,25 @@ class Node:
         u2=v*(self.wheel_axis_l/self.wheel_r)
         u1=-u2
         self.spinwheels(u1,u2,time)
-def init_rcnn()
-	args = parse_args()
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--cpu', dest='cpu_mode',
+                        help='Use CPU mode (overrides --gpu)',
+                        action='store_true')
+    #parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
+    #                    choices=NETS.keys(), default='vgg16')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [zf]',
+                        choices=NETS.keys(), default='zf')
+
+    args = parser.parse_args()
+
+    return args
+def init_rcnn():
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+    args = parse_args()
     prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
                             'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
     #print prototxt
@@ -266,17 +351,30 @@ def init_rcnn()
         raise IOError(('{:s} not found.\nDid you run ./data/script/'
                        'fetch_faster_rcnn_models.sh?').format(caffemodel))
 
-    if args.cpu_mode:
-        caffe.set_mode_cpu()
-    else:
-        caffe.set_mode_gpu()
-        caffe.set_device(args.gpu_id)
-        cfg.GPU_ID = args.gpu_id
+
+    #if args.cpu_mode:
+    #    caffe.set_mode_cpu()
+    #else:
+    caffe.set_mode_gpu()
+    caffe.set_device(args.gpu_id)
+    cfg.GPU_ID = args.gpu_id
     net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+    for i in xrange(2):
+        _, _= im_detect(net, im)
+
+    im_file = os.path.join(cfg.DATA_DIR, 'demo', '001763.jpg')
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(net, im)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+    return net
 
 if __name__ == "__main__":
-	init_rcnn()
-    rospy.init_node("lab2_example")
+    net = None
+    rospy.init_node("video_tracking")
     node = Node()
     #this function loops and returns when the node shuts down
     #all logic should occur in the callback function
